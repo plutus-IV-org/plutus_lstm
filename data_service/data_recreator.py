088d433c7c4a5cb9854a58eaab38253c64651f13
IlliaBoloto <83161286+IlliaBoloto@users.noreply.github.com>
2023-07-26 15:29:21 +0200

    applied target orientation for run_command_4

--- data_service/data_recreator.py
+++ data_service/data_recreator.py
@@ -4,6 +4,7 @@ import os
 from tensorflow.keras.models import Sequential, load_model
 from data_service.data_preparation import DataPreparation
 from data_service.data_transformation import _data_normalisation, _data_denormalisation, _split_data
+
 pd.options.mode.chained_assignment = None
 from PATH_CONFIG import _ROOT_PATH
 from utilities.service_functions import _slash_conversion
@@ -29,45 +30,51 @@ def directional_loss(y_true, y_pred):
 
     return loss
 
-def data_preparation(cluster:str):
-    t1=dt.datetime.now()
+
+def data_preparation(cluster: str):
+    t1 = dt.datetime.now()
     sl = _slash_conversion()
-    path = _ROOT_PATH() +  sl + 'vaults' + sl + 'cluster' + sl + cluster
+    path = _ROOT_PATH() + sl + 'vaults' + sl + 'cluster' + sl + cluster
     models_names = []
-    scope_lst =[]
+    scope_lst = []
     for m in os.listdir(path):
         scope_lst.append(m.split('_')[0:2])
         models_names.append(m)
-    #Common table required as the 1st output form dict
+    # Common table required as the 1st output form dict
     ct = pd.DataFrame()
-    #Creating main dict
+    # Creating main dict
     pred_saver = {}
-    for a,b in zip(models_names,scope_lst):
-        dp = DataPreparation(b[0],b[1],a.split('_')[9], a.split('_')[5],a.split('_')[3])
+    for a, b in zip(models_names, scope_lst):
+        dp = DataPreparation(b[0], b[1], a.split('_')[9], a.split('_')[5], a.split('_')[3])
         data = dp._download_prices()
-        if b[1]== "Custom":
+        if b[1] == "Custom":
             lstm_research_dict_path = path + sl + a + sl + 'lstm_research_dict.pickle'
             with open(lstm_research_dict_path, "rb") as f:
                 loaded_dict = pickle.load(f)
             custom_indicators = loaded_dict['data_table'].columns.tolist()
+            # TODO add apply means
             data = data[custom_indicators]
             data = data.dropna()
             if len(data) < 1500:
                 raise Exception('Too short selected data')
-        #data = _download_prices(b[0], 'Close')
+        # data = _download_prices(b[0], 'Close')
         sep = a.split('_')
         f_d = int(sep[4])
         p_d = int(sep[3])
-        #common_table = _download_prices(b[0], 'Open').rename(columns={'Open': sep[0]})
+        # common_table = _download_prices(b[0], 'Open').rename(columns={'Open': sep[0]})
         common_table = data[["Close"]].rename(columns={'Close': sep[0]})
-        ct = pd.concat([ct, common_table],axis =1)
+        ct = pd.concat([ct, common_table], axis=1)
         df = data.dropna()
         df_prices = df.copy().iloc[-1000:]
 
+        if a.split('_')[-1] == 'T':
+            targeted = True
+        else:
+            targeted = False
         # Normalisation
-        normalised_data = _data_normalisation(df_prices)
-        trainX, trainY, testX, testY= _split_data(normalised_data, f_d, p_d,break_point=False)
-        #Loading model
+        normalised_data = _data_normalisation(df_prices, is_targeted=targeted)
+        trainX, trainY, testX, testY = _split_data(normalised_data, f_d, p_d, break_point=False, is_targeted=targeted)
+        # Loading model
         absolute_path = path + '\\' + a + '\\LSTM.h5'
         try:
             model = load_model(absolute_path)
@@ -76,24 +83,21 @@ def data_preparation(cluster:str):
             model = load_model(absolute_path, custom_objects={'directional_loss': directional_loss})
             predictions = model.predict(testX, verbose=0)
 
-        denormalised = _data_denormalisation(predictions.copy(), df_prices, f_d,testY, break_point=False)
+        denormalised = _data_denormalisation(predictions.copy(), df_prices, f_d, testY, break_point=False,
+                                             is_targeted=targeted)
         time_index = normalised_data.tail(len(denormalised)).index
-        df_pred = pd.DataFrame(denormalised, index = time_index)
-        pred_saver[a] =common_table.iloc[-126:],df_pred.iloc[-126:]
+        df_pred = pd.DataFrame(denormalised, index=time_index)
+        if targeted:
+            constructed_df = pd.DataFrame()
+            for t in range(f_d):
+                constructed_df = pd.concat([constructed_df, df_prices.loc[df_pred.index, 'Close']], axis=1)
+            sd = constructed_df.std().values[0]
+            df_pred = constructed_df.loc[df_pred.index].values + (df_pred * sd)
+
+        pred_saver[a] = common_table.iloc[-126:], df_pred.iloc[-126:]
         interval = a.split('_')[5]
 
-    #Common table
-    #df = ct.loc[:,~ct.columns.duplicated()].copy()
-    #for x in pred_saver.keys():
-    #    pred_saver[x] = common_table.iloc[-126:],pred_saver[x]
     t2 = dt.datetime.now()
     delta = t2 - t1
     print(delta)
     return pred_saver, interval
-
-
-
-
-
-
-
