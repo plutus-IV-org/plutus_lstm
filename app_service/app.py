Fixed bug with average models and directional accuracy calcs.

app_service/app.py
@@ -7,15 +7,19 @@ from app_service.input_data_maker import generate_data
 from flask import Flask
 import logging
 from app_service.visualise_predictions import add_prediction
+from db_service.SQLite import directional_accuracy_history_load
 from app_service.app_utilities import *
 import socket
 from contextlib import closing
 from playsound import playsound
+from Const import DA_TABLE
+
 app = Flask(__name__)
 
 log = logging.getLogger('werkzeug')
 log.disabled = True
 sound_path = r"C:\Users\ilsbo\PycharmProjects\plutus_lstm\Notifications\Sound\run_command_report_generated.mp3"
+
 
 def find_free_port(start_port, end_port):
     for port in range(start_port, end_port):
@@ -218,33 +222,59 @@ def generate_app(cluster):
 
         fig = sp.make_subplots(rows=num_relevant_models * 2,
                                cols=1,
-                               shared_xaxes=True,
+                               shared_xaxes=False,
                                subplot_titles=(subplot_names))
 
-        div_fig_row = 1
+        history_score_fig_row = 1
         diff_fig_row = 2
 
         for model in relevant_models:
             deviation_data, deviation_data_diff = auxiliary_dataframes(model, asset_prices, selected_dict,
                                                                        asset_name, anomalies_toggle, anomalies_window,
                                                                        rolling_period, zscore_lvl)
+            df = directional_accuracy_history_load(DA_TABLE)
+            selected_df = df[df['model_name'] == model]
+            grouped_df = group_by_history_score(selected_df)
+            # Load the DataFrame and extract unique model names
+            # history_score_fig = go.Scatter(x=grouped_df['date'],
+            #                                y=grouped_df['da_score'],
+            #                                mode='lines',  # Use 'markers' for scatter plot
+            #                                marker=dict(
+            #                                    color=grouped_df['da_score'],
+            #                                    # Color of markers based on da_score values
+            #                                    colorscale='magma',  # You can choose any supported colorscale
+            #
+            #                                ))
+
+            history_score_fig = go.Bar(
+                x=grouped_df['date'],
+                y=grouped_df['da_score'],
+                marker=dict(
+                    color=grouped_df['da_score'],
+                    cmin=0,
+                    cmax=1,  # Set the color of the bars based on the da_score
+                    colorscale='Turbo',  # Or any other color scale that you find eye-friendly
+                )
+            )
 
             div_fig = go.Heatmap(z=deviation_data.values,
                                  x=deviation_data.columns,
                                  y=deviation_data.index,
                                  colorscale='Reds',
                                  showscale=False)
-
+            #round(deviation_data_diff.mean(axis=1), 2).values.tolist()
             diff_fig = go.Heatmap(z=deviation_data_diff.values,
                                   x=deviation_data_diff.columns,
                                   y=deviation_data_diff.index,
                                   colorscale='BuGn',
                                   showscale=False)
 
-            fig.add_trace(div_fig, row=div_fig_row, col=1)
+            # fig.add_trace(div_fig, row=div_fig_row, col=1)
+            fig.add_trace(history_score_fig, row=history_score_fig_row, col=1)
             fig.add_trace(diff_fig, row=diff_fig_row, col=1)
 
-            div_fig_row += 2
+            # div_fig_row += 2
+            history_score_fig_row += 2
             diff_fig_row += 2
 
         fig.update_layout(height=dynamic_height(num_relevant_models), title_text="Models performance")

utilities/cluster_common_line.py
@@ -87,140 +87,140 @@ def compute_averages(initial_keys: List[str], data: Dict) -> Tuple[List[str], Di
                 data[new_key] = data[sample_key_for_interval][0], weighted_average_df
                 initial_keys.append(new_key)
 
-                # Long mean
-                # Loop through each batch in LongsBatches
-                for x in range(len(list(LongsBatches))):
-                    # Check if the current batch matches the specified interval and number of columns
-                    if list(LongsBatches)[x].name.split('_')[1] == interval and \
-                            list(LongsBatches)[x].name.split('_')[2] == str(number_of_columns):
-                        # Process the batch if it has elements
-                        if len(list(LongsBatches)[x].value) > 0:
-                            # Initialize dictionaries for storing predictions and weights
-                            long_storage = {}
-                            weights_storage = []
-                            position_key = 1
-                            # Iterate over short model names in the batch
-                            for short_model_name in list(LongsBatches)[x].value.keys():
-                                # Loop through each key in the data
-                                for key in data.keys():
-                                    # Construct a regex pattern to match the short_model_name
-                                    pattern = f".*{re.escape(short_model_name)}.*"
-                                    # If the pattern matches the key, process the data
-                                    if re.match(pattern, key):
-                                        prediction_df = data[key][1]
-                                        # Retrieve the weight for the current model
-                                        weight = list(LongsBatches)[x].value[short_model_name]
-                                        # Store the prediction dataframe in long_storage
-                                        long_storage[position_key] = prediction_df
-                                        # Append the weight to the weights storage
-                                        weights_storage.append(weight)
-                                        # Increment the position key
-                                        position_key += 1
-                                        break
-
-                            # Convert weights list to a NumPy array for processing
-                            weights_storage_array = np.array(weights_storage)
-                            # Calculate the weighted mean prediction
-                            long_mean = count_mean_prediction_line(long_storage, weights=weights_storage_array)
-
-                            # Construct a new key for storing the long mean in data
-                            new_key = f"{name}_long-average_{column_count}_future_steps_{interval}"
-                            # Find a sample key for the interval
-                            sample_key_for_interval = next(filter(lambda x: interval in x, initial_keys))
-                            # Store the long mean in data with the new key
-                            data[new_key] = data[sample_key_for_interval][0], long_mean
-                            # Append the new key to initial_keys for future reference
-                            initial_keys.append(new_key)
-
-                # Short mean
-                # Loop through each batch in ShortssBatches
-                for x in range(len(list(ShortsBatches))):
-                    # Check if the current batch matches the specified interval and number of columns
-                    if list(ShortsBatches)[x].name.split('_')[1] == interval and \
-                            list(ShortsBatches)[x].name.split('_')[2] == str(number_of_columns):
-                        # Process the batch if it has elements
-                        if len(list(ShortsBatches)[x].value) > 0:
-                            # Initialize dictionaries for storing predictions and weights
-                            short_storage = {}
-                            weights_storage = []
-                            position_key = 1
-                            # Iterate over short model names in the batch
-                            for short_model_name in list(ShortsBatches)[x].value.keys():
-                                # Loop through each key in the data
-                                for key in data.keys():
-                                    # Construct a regex pattern to match the short_model_name
-                                    pattern = f".*{re.escape(short_model_name)}.*"
-                                    # If the pattern matches the key, process the data
-                                    if re.match(pattern, key):
-                                        prediction_df = data[key][1]
-                                        # Retrieve the weight for the current model
-                                        weight = list(ShortsBatches)[x].value[short_model_name]
-                                        # Store the prediction dataframe in short_storage
-                                        short_storage[position_key] = prediction_df
-                                        # Append the weight to the weights storage
-                                        weights_storage.append(weight)
-                                        # Increment the position key
-                                        position_key += 1
-                                        break
-
-                            # Convert weights list to a NumPy array for processing
-                            weights_storage_array = np.array(weights_storage)
-                            # Calculate the weighted mean prediction
-                            short_mean = count_mean_prediction_line(short_storage, weights=weights_storage_array)
-
-                            # Construct a new key for storing the short mean in data
-                            new_key = f"{name}_short-average_{column_count}_future_steps_{interval}"
-                            # Find a sample key for the interval
-                            sample_key_for_interval = next(filter(lambda x: interval in x, initial_keys))
-                            # Store the short mean in data with the new key
-                            data[new_key] = data[sample_key_for_interval][0], short_mean
-                            # Append the new key to initial_keys for future reference
-                            initial_keys.append(new_key)
-
-                # Top mean
-                # Loop through each batch in TopBatches
-                for x in range(len(list(ModelBatches))):
-                    # Check if the current batch matches the specified interval and number of columns
-                    if list(ModelBatches)[x].name.split('_')[1] == interval and \
-                            list(ModelBatches)[x].name.split('_')[2] == str(number_of_columns):
-                        # Process the batch if it has elements
-                        if len(list(ModelBatches)[x].value) > 0:
-                            # Initialize dictionaries for storing predictions and weights
-                            top_storage = {}
-                            weights_storage = []
-                            position_key = 1
-                            # Iterate over top model names in the batch
-                            for top_model_name in list(ModelBatches)[x].value.keys():
-                                # Loop through each key in the data
-                                for key in data.keys():
-                                    # Construct a regex pattern to match the top_model_name
-                                    pattern = f".*{re.escape(top_model_name)}.*"
-                                    # If the pattern matches the key, process the data
-                                    if re.match(pattern, key):
-                                        prediction_df = data[key][1]
-                                        # Retrieve the weight for the current model
-                                        weight = list(ModelBatches)[x].value[top_model_name]
-                                        # Store the prediction dataframe in top_storage
-                                        top_storage[position_key] = prediction_df
-                                        # Append the weight to the weights storage
-                                        weights_storage.append(weight)
-                                        # Increment the position key
-                                        position_key += 1
-                                        break
-
-                            # Convert weights list to a NumPy array for processing
-                            weights_storage_array = np.array(weights_storage)
-                            # Calculate the weighted mean prediction
-                            top_mean = count_mean_prediction_line(top_storage, weights=weights_storage_array)
-
-                            # Construct a new key for storing the top mean in data
-                            new_key = f"{name}_top-average_{column_count}_future_steps_{interval}"
-                            # Find a sample key for the interval
-                            sample_key_for_interval = next(filter(lambda x: interval in x, initial_keys))
-                            # Store the top mean in data with the new key
-                            data[new_key] = data[sample_key_for_interval][0], top_mean
-                            # Append the new key to initial_keys for future reference
-                            initial_keys.append(new_key)
+                # # Long mean
+                # # Loop through each batch in LongsBatches
+                # for x in range(len(list(LongsBatches))):
+                #     # Check if the current batch matches the specified interval and number of columns
+                #     if list(LongsBatches)[x].name.split('_')[1] == interval and \
+                #             list(LongsBatches)[x].name.split('_')[2] == str(column_count):
+                #         # Process the batch if it has elements
+                #         if len(list(LongsBatches)[x].value) > 0:
+                #             # Initialize dictionaries for storing predictions and weights
+                #             long_storage = {}
+                #             weights_storage = []
+                #             position_key = 1
+                #             # Iterate over short model names in the batch
+                #             for short_model_name in list(LongsBatches)[x].value.keys():
+                #                 # Loop through each key in the data
+                #                 for key in data.keys():
+                #                     # Construct a regex pattern to match the short_model_name
+                #                     pattern = f".*{re.escape(short_model_name)}.*"
+                #                     # If the pattern matches the key, process the data
+                #                     if re.match(pattern, key):
+                #                         prediction_df = data[key][1]
+                #                         # Retrieve the weight for the current model
+                #                         weight = list(LongsBatches)[x].value[short_model_name]
+                #                         # Store the prediction dataframe in long_storage
+                #                         long_storage[position_key] = prediction_df
+                #                         # Append the weight to the weights storage
+                #                         weights_storage.append(weight)
+                #                         # Increment the position key
+                #                         position_key += 1
+                #                         break
+                #
+                #             # Convert weights list to a NumPy array for processing
+                #             weights_storage_array = np.array(weights_storage)
+                #             # Calculate the weighted mean prediction
+                #             long_mean = count_mean_prediction_line(long_storage, weights=weights_storage_array)
+                #
+                #             # Construct a new key for storing the long mean in data
+                #             new_key = f"{name}_long-average_{column_count}_future_steps_{interval}"
+                #             # Find a sample key for the interval
+                #             sample_key_for_interval = next(filter(lambda x: interval in x, initial_keys))
+                #             # Store the long mean in data with the new key
+                #             data[new_key] = data[sample_key_for_interval][0], long_mean
+                #             # Append the new key to initial_keys for future reference
+                #             initial_keys.append(new_key)
+                #
+                # # Short mean
+                # # Loop through each batch in ShortssBatches
+                # for x in range(len(list(ShortsBatches))):
+                #     # Check if the current batch matches the specified interval and number of columns
+                #     if list(ShortsBatches)[x].name.split('_')[1] == interval and \
+                #             list(ShortsBatches)[x].name.split('_')[2] == str(column_count):
+                #         # Process the batch if it has elements
+                #         if len(list(ShortsBatches)[x].value) > 0:
+                #             # Initialize dictionaries for storing predictions and weights
+                #             short_storage = {}
+                #             weights_storage = []
+                #             position_key = 1
+                #             # Iterate over short model names in the batch
+                #             for short_model_name in list(ShortsBatches)[x].value.keys():
+                #                 # Loop through each key in the data
+                #                 for key in data.keys():
+                #                     # Construct a regex pattern to match the short_model_name
+                #                     pattern = f".*{re.escape(short_model_name)}.*"
+                #                     # If the pattern matches the key, process the data
+                #                     if re.match(pattern, key):
+                #                         prediction_df = data[key][1]
+                #                         # Retrieve the weight for the current model
+                #                         weight = list(ShortsBatches)[x].value[short_model_name]
+                #                         # Store the prediction dataframe in short_storage
+                #                         short_storage[position_key] = prediction_df
+                #                         # Append the weight to the weights storage
+                #                         weights_storage.append(weight)
+                #                         # Increment the position key
+                #                         position_key += 1
+                #                         break
+                #
+                #             # Convert weights list to a NumPy array for processing
+                #             weights_storage_array = np.array(weights_storage)
+                #             # Calculate the weighted mean prediction
+                #             short_mean = count_mean_prediction_line(short_storage, weights=weights_storage_array)
+                #
+                #             # Construct a new key for storing the short mean in data
+                #             new_key = f"{name}_short-average_{column_count}_future_steps_{interval}"
+                #             # Find a sample key for the interval
+                #             sample_key_for_interval = next(filter(lambda x: interval in x, initial_keys))
+                #             # Store the short mean in data with the new key
+                #             data[new_key] = data[sample_key_for_interval][0], short_mean
+                #             # Append the new key to initial_keys for future reference
+                #             initial_keys.append(new_key)
+                #
+                # # Top mean
+                # # Loop through each batch in TopBatches
+                # for x in range(len(list(ModelBatches))):
+                #     # Check if the current batch matches the specified interval and number of columns
+                #     if list(ModelBatches)[x].name.split('_')[1] == interval and \
+                #             list(ModelBatches)[x].name.split('_')[2] == str(column_count):
+                #         # Process the batch if it has elements
+                #         if len(list(ModelBatches)[x].value) > 0:
+                #             # Initialize dictionaries for storing predictions and weights
+                #             top_storage = {}
+                #             weights_storage = []
+                #             position_key = 1
+                #             # Iterate over top model names in the batch
+                #             for top_model_name in list(ModelBatches)[x].value.keys():
+                #                 # Loop through each key in the data
+                #                 for key in data.keys():
+                #                     # Construct a regex pattern to match the top_model_name
+                #                     pattern = f".*{re.escape(top_model_name)}.*"
+                #                     # If the pattern matches the key, process the data
+                #                     if re.match(pattern, key):
+                #                         prediction_df = data[key][1]
+                #                         # Retrieve the weight for the current model
+                #                         weight = list(ModelBatches)[x].value[top_model_name]
+                #                         # Store the prediction dataframe in top_storage
+                #                         top_storage[position_key] = prediction_df
+                #                         # Append the weight to the weights storage
+                #                         weights_storage.append(weight)
+                #                         # Increment the position key
+                #                         position_key += 1
+                #                         break
+                #
+                #             # Convert weights list to a NumPy array for processing
+                #             weights_storage_array = np.array(weights_storage)
+                #             # Calculate the weighted mean prediction
+                #             top_mean = count_mean_prediction_line(top_storage, weights=weights_storage_array)
+                #
+                #             # Construct a new key for storing the top mean in data
+                #             new_key = f"{name}_top-average_{column_count}_future_steps_{interval}"
+                #             # Find a sample key for the interval
+                #             sample_key_for_interval = next(filter(lambda x: interval in x, initial_keys))
+                #             # Store the top mean in data with the new key
+                #             data[new_key] = data[sample_key_for_interval][0], top_mean
+                #             # Append the new key to initial_keys for future reference
+                #             initial_keys.append(new_key)
 
                 # Adjusted mean
                 # run_times = 100  # You can adjust this value